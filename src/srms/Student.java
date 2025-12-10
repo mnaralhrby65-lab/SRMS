e38f035ad603b71f6729174bcebc90f01b0307e0
n4777 <n4777@NORA>
2025-12-10 03:21:15 +0300

    Added Student Add and Display feature

--- src/srms/Student.java
+++ src/srms/Student.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package srms;
 
-/**
- *
- * @author mnara
- */
 public class Student {
     
+    private final String name;
+    private final String studentID;
+    private final Department department;
+    private final  int year;
+    private final double gpa;
+
+    public Student(String name, String studentID, Department department, int year, double gpa) {
+        this.name = name;
+        this.studentID = studentID;
+        this.department = department;
+        this.year = year;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+    
+    public int getYear() {
+        return year;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+    
+    @Override
+    public String toString() {
+        return "Name: " + name + 
+               " | ID: " + studentID + 
+               " | Dept: " + department.getDeptName() + 
+               " | Year: " + year + 
+               " | GPA: " + gpa;
+    }
 }
