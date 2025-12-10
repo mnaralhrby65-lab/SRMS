7981dc234dccbe8129ca40aafbc200c200192687
n4777 <n4777@NORA>
2025-12-10 03:18:46 +0300

    Added Student Add and Display feature

---
+++ src/srms/StudentManagement.java
@@ -0,0 +1,31 @@
+package srms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentManagement {
+    
+    private List<Student> students;
+
+    public StudentManagement() {
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+        System.out.println("✅ Student: " + student.getName() + " added successfully.");
+    }
+
+    public void displayAllStudents() {
+        if (students.isEmpty()) {
+            System.out.println("ℹ️ No students currently registered.");
+            return;
+        }
+
+        System.out.println("\n--- Registered Students List ---");
+        for (Student student : students) {
+            System.out.println(student); 
+        }
+        System.out.println("--------------------------------\n");
+    }
+}
